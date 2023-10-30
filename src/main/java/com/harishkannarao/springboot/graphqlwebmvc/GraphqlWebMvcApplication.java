730ad74510e04abe277d486abb1dee6b51394c54
harishkannarao <harishkannarao@users.noreply.github.com>
2023-10-30 22:33:17 +0000

    Initial commit from Spring Intializer

---
+++ src/main/java/com/harishkannarao/springboot/graphqlwebmvc/GraphqlWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.springboot.graphqlwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/GraphqlWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.springboot.graphqlwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
