94e480e645b710c450c53a722b40a4947551594a
harishkannarao <harishkannarao@users.noreply.github.com>
2024-02-29 17:21:29 +0000

    Using dynamic fixed port for first test

--- src/test/java/com/harishkannarao/springboot/graphqlwebmvc/AbstractBaseIntegrationTest.java
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/AbstractBaseIntegrationTest.java
@@ -1,7 +1,20 @@
 package com.harishkannarao.springboot.graphqlwebmvc;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.util.TestSocketUtils;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+@ActiveProfiles("it")
 public abstract class AbstractBaseIntegrationTest {
+
+    @DynamicPropertySource
+    static void registerTestProperties(DynamicPropertyRegistry registry) {
+        final int RANDOM_SERVER_PORT = TestSocketUtils.findAvailableTcpPort();
+        registry.add("server.port", () -> String.valueOf(RANDOM_SERVER_PORT));
+    }
 }
--- src/test/java/com/harishkannarao/springboot/graphqlwebmvc/graphql/GreetingQueryTest.java
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/graphql/GreetingQueryTest.java
@@ -2,11 +2,24 @@ package com.harishkannarao.springboot.graphqlwebmvc.graphql;
 
 import com.harishkannarao.springboot.graphqlwebmvc.AbstractBaseIntegrationTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GreetingQueryTest extends AbstractBaseIntegrationTest {
 
+    private final String graphqlUrl;
+
+    @Autowired
+    public GreetingQueryTest(
+            @Value("${test.application.graphqlUrl}") String graphqlUrl
+    ) {
+        this.graphqlUrl = graphqlUrl;
+    }
+
     @Test
     public void test_greeting_query_with_parameters() {
-
+        assertThat(graphqlUrl).isNotBlank();
     }
 }
