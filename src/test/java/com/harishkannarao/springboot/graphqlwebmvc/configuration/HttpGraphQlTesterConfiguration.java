6b433ccef582c47bed22e0e89182685eca770cfe
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-03 22:23:01 +0000

    Enabling wiretapping on http graphql test client

--- src/test/java/com/harishkannarao/springboot/graphqlwebmvc/configuration/HttpGraphQlTesterConfiguration.java
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/configuration/HttpGraphQlTesterConfiguration.java
@@ -1,10 +1,17 @@
 package com.harishkannarao.springboot.graphqlwebmvc.configuration;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class HttpGraphQlTesterConfiguration {
@@ -12,9 +19,13 @@ public class HttpGraphQlTesterConfiguration {
     public HttpGraphQlTester createHttpGrqphQlTester(
             @Value("${test.application.graphqlUrl}") String graphqlUrl
     ) {
+        final HttpClient wiretappedHttpClient = HttpClient.create()
+                .wiretap("reactor.netty.http.client.HttpClient",
+                        LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
         WebTestClient client =
                 WebTestClient.bindToServer()
                         .baseUrl(graphqlUrl)
+                        .clientConnector(new ReactorClientHttpConnector(wiretappedHttpClient))
                         .build();
 
         return HttpGraphQlTester.create(client);
