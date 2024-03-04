3ad9c14dfe61b6013a997cc8b6fc44ec40883ac5
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-04 16:37:42 +0000

    Using MDC to read value in the controller

--- src/main/java/com/harishkannarao/springboot/graphqlwebmvc/controller/GreetingController.java
+++ src/main/java/com/harishkannarao/springboot/graphqlwebmvc/controller/GreetingController.java
@@ -1,5 +1,9 @@
 package com.harishkannarao.springboot.graphqlwebmvc.controller;
 
+import com.harishkannarao.springboot.graphqlwebmvc.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -7,10 +11,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class GreetingController {
 
-    @QueryMapping(name = "greeting")
-    public String handleGreeting(
-            @Argument(name = "name")
-            String inputName) {
-        return "Hello, " + inputName + "!";
-    }
+	private final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
+	@QueryMapping(name = "greeting")
+	public String handleGreeting(
+		@Argument(name = "name")
+		String inputName) {
+		logger.info("Generating greeting for input {} with request id {}",
+			inputName, MDC.get(Constants.X_REQUEST_ID));
+		return "Hello, " + inputName + "!";
+	}
 }
