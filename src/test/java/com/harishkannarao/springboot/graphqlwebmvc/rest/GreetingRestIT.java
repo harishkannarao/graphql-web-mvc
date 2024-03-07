19c7459feb7e3870ce373d087708a75f3c490c03
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-07 15:59:03 +0000

    Adding request id to headers

--- src/test/java/com/harishkannarao/springboot/graphqlwebmvc/rest/GreetingRestIT.java
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/rest/GreetingRestIT.java
@@ -2,13 +2,18 @@ package com.harishkannarao.springboot.graphqlwebmvc.rest;
 
 import com.harishkannarao.springboot.graphqlwebmvc.AbstractBaseIT;
 import com.harishkannarao.springboot.graphqlwebmvc.model.GreetingResponseDto;
+import com.harishkannarao.springboot.graphqlwebmvc.util.Constants;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 
 import java.util.Objects;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,13 +41,18 @@ public class GreetingRestIT extends AbstractBaseIT {
 	@Test
 	public void get_greeting_returns_400_bad_request() {
 		final String inputName = "throw-error";
+		final String requestId = UUID.randomUUID().toString();
 
+		final HttpHeaders reqHeaders = new HttpHeaders();
+		reqHeaders.add(Constants.X_REQUEST_ID, requestId);
+		final HttpEntity<Void> reqEntity = new HttpEntity<>(reqHeaders);
 		final ResponseEntity<String> result = testRestTemplate
-			.getForEntity("/rest/greeting?name=" + inputName, String.class);
+			.exchange("/rest/greeting?name=" + inputName, HttpMethod.GET, reqEntity, String.class);
 
 		assertThat(result.getStatusCode().value()).isEqualTo(400);
 		assertThat(result.getBody())
 			.contains("Artificial Error !!!")
+			.contains(requestId)
 			.contains("Bad Request")
 			.contains("400")
 			.contains("/graphql-web-mvc/rest/greeting");
