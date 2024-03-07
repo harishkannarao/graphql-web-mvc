64db94f8480816178dac2b40a7b4e80f7f4d358a
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-07 14:33:26 +0000

    Introducing rest client access logging interceptor

---
+++ src/main/java/com/harishkannarao/springboot/graphqlwebmvc/client/rest/RestClientAccessLoggingInterceptor.java
@@ -0,0 +1,31 @@
+package com.harishkannarao.springboot.graphqlwebmvc.client.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class RestClientAccessLoggingInterceptor implements ClientHttpRequestInterceptor {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestClientAccessLoggingInterceptor.class);
+
+	@Override
+	public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] body, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+		long startTime = System.currentTimeMillis();
+		int statusCode = 0;
+		String method = httpRequest.getMethod().name();
+		String url = httpRequest.getURI().toString();
+		ClientHttpResponse response;
+		try {
+			response = clientHttpRequestExecution.execute(httpRequest, body);
+			statusCode = response.getStatusCode().value();
+		} finally {
+			long timeTaken = System.currentTimeMillis() - startTime;
+			LOGGER.info("REST_CLIENT_ACCESS_LOG {} {} {} {}", timeTaken, statusCode, method, url);
+		}
+		return response;
+	}
+}
--- src/test/java/com/harishkannarao/springboot/graphqlwebmvc/configuration/TestRestTemplateConfiguration.java
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/configuration/TestRestTemplateConfiguration.java
@@ -1,18 +1,31 @@
 package com.harishkannarao.springboot.graphqlwebmvc.configuration;
 
+import com.harishkannarao.springboot.graphqlwebmvc.client.rest.RestClientAccessLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
+import java.time.Duration;
+import java.util.List;
 
 @Configuration
 public class TestRestTemplateConfiguration {
 
-    @Bean
-    public TestRestTemplate createTestRestTemplate(
-            @Value("${test.application.baseUrl}") String rootUrl
-    ) {
-        return new TestRestTemplate(new RestTemplateBuilder().rootUri(rootUrl));
-    }
+	@Bean
+	public TestRestTemplate createTestRestTemplate(
+		@Value("${test.application.baseUrl}") String rootUrl
+	) {
+		SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+		BufferingClientHttpRequestFactory clientHttpRequestFactory =
+			new BufferingClientHttpRequestFactory(simpleClientHttpRequestFactory);
+		return new TestRestTemplate(new RestTemplateBuilder()
+			.rootUri(rootUrl)
+			.requestFactory(() -> clientHttpRequestFactory)
+			.additionalInterceptors(List.of(new RestClientAccessLoggingInterceptor()))
+		);
+	}
 }
