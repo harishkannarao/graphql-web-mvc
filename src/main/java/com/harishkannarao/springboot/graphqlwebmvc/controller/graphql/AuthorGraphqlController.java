8afb57502104a44fc3a9a16722ac5b0772a89225
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-15 16:33:58 +0000

    Creating type definition for authors and books

--- src/main/java/com/harishkannarao/springboot/graphqlwebmvc/controller/graphql/AuthorGraphqlController.java
+++ src/main/java/com/harishkannarao/springboot/graphqlwebmvc/controller/graphql/AuthorGraphqlController.java
@@ -1,19 +1,39 @@
 package com.harishkannarao.springboot.graphqlwebmvc.controller.graphql;
 
+import com.harishkannarao.springboot.graphqlwebmvc.dao.AuthorDao;
+import com.harishkannarao.springboot.graphqlwebmvc.dao.BookAuthorDao;
+import com.harishkannarao.springboot.graphqlwebmvc.dao.entity.DbEntity;
 import com.harishkannarao.springboot.graphqlwebmvc.model.Author;
 import com.harishkannarao.springboot.graphqlwebmvc.model.Book;
-import org.springframework.graphql.data.method.annotation.Argument;
+import com.harishkannarao.springboot.graphqlwebmvc.model.BookAuthor;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 @Controller
 public class AuthorGraphqlController {
 
+	private final BookAuthorDao bookAuthorDao;
+	private final AuthorDao authorDao;
+
+	public AuthorGraphqlController(
+		BookAuthorDao bookAuthorDao,
+		AuthorDao authorDao) {
+		this.bookAuthorDao = bookAuthorDao;
+		this.authorDao = authorDao;
+	}
+
 	@SchemaMapping(typeName = "Book", field = "authors")
 	public List<Author> listAuthors(final Book book) {
-		return List.of(new Author(UUID.randomUUID().toString(), "author-" + UUID.randomUUID()));
+		List<String> authorIds = bookAuthorDao.listByBookIds(List.of(book.id()))
+			.stream()
+			.map(DbEntity::data)
+			.map(BookAuthor::authorId)
+			.toList();
+		return authorIds.isEmpty()
+			? Collections.emptyList()
+			: authorDao.list(authorIds).stream().map(DbEntity::data).toList();
 	}
 }
