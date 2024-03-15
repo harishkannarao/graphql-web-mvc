ebe1df027feb3d04dc740da96302d942a373702a
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-15 16:39:56 +0000

    Using batch mapping instead of schema mapping for getting authors

--- src/main/java/com/harishkannarao/springboot/graphqlwebmvc/controller/graphql/AuthorGraphqlController.java
+++ src/main/java/com/harishkannarao/springboot/graphqlwebmvc/controller/graphql/AuthorGraphqlController.java
@@ -6,11 +6,14 @@ import com.harishkannarao.springboot.graphqlwebmvc.dao.entity.DbEntity;
 import com.harishkannarao.springboot.graphqlwebmvc.model.Author;
 import com.harishkannarao.springboot.graphqlwebmvc.model.Book;
 import com.harishkannarao.springboot.graphqlwebmvc.model.BookAuthor;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class AuthorGraphqlController {
@@ -25,15 +28,20 @@ public class AuthorGraphqlController {
 		this.authorDao = authorDao;
 	}
 
-	@SchemaMapping(typeName = "Book", field = "authors")
-	public List<Author> listAuthors(final Book book) {
-		List<String> authorIds = bookAuthorDao.listByBookIds(List.of(book.id()))
-			.stream()
-			.map(DbEntity::data)
-			.map(BookAuthor::authorId)
-			.toList();
-		return authorIds.isEmpty()
-			? Collections.emptyList()
-			: authorDao.list(authorIds).stream().map(DbEntity::data).toList();
+	@BatchMapping(typeName = "Book", field = "authors", maxBatchSize = 100)
+	public Map<Book, List<Author>> listAuthors(final List<Book> books) {
+		return books.stream()
+			.map(book -> {
+				List<String> authorIds = bookAuthorDao.listByBookIds(List.of(book.id()))
+					.stream()
+					.map(DbEntity::data)
+					.map(BookAuthor::authorId)
+					.toList();
+				List<Author> authors = authorIds.isEmpty()
+					? Collections.emptyList()
+					: authorDao.list(authorIds).stream().map(DbEntity::data).toList();
+				return Map.entry(book, authors);
+			})
+			.collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
 	}
 }
