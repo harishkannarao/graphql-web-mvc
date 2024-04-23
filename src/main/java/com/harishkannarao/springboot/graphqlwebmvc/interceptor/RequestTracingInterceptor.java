a7a8134e70c4cc8b63a6322252c32d51bc90acd0
harishkannarao <harishkannarao@users.noreply.github.com>
2024-04-23 22:10:15 +0100

    Fixing a warning

--- src/main/java/com/harishkannarao/springboot/graphqlwebmvc/interceptor/RequestTracingInterceptor.java
+++ src/main/java/com/harishkannarao/springboot/graphqlwebmvc/interceptor/RequestTracingInterceptor.java
@@ -1,6 +1,7 @@
 package com.harishkannarao.springboot.graphqlwebmvc.interceptor;
 
 import com.harishkannarao.springboot.graphqlwebmvc.util.Constants;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.graphql.server.WebGraphQlInterceptor;
 import org.springframework.graphql.server.WebGraphQlRequest;
 import org.springframework.graphql.server.WebGraphQlResponse;
@@ -10,7 +11,7 @@ import java.util.Collections;
 
 public class RequestTracingInterceptor implements WebGraphQlInterceptor {
 	@Override
-	public Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
+	public @NotNull Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
 		String value = String.valueOf(request.getAttributes().get(Constants.X_REQUEST_ID));
 		request.configureExecutionInput((executionInput, builder) ->
 			builder.graphQLContext(Collections.singletonMap(Constants.X_REQUEST_ID, value)).build());
