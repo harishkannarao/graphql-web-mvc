98902d78a05a148fe7efe511111173a2befed7bd
harishkannarao <harishkannarao@users.noreply.github.com>
2024-05-29 14:33:35 +0100

    Fixing ApplicationLocalRunner

--- src/test/java/com/harishkannarao/springboot/graphqlwebmvc/runner/ApplicationLocalRunner.java
+++ src/test/java/com/harishkannarao/springboot/graphqlwebmvc/runner/ApplicationLocalRunner.java
@@ -3,6 +3,7 @@ package com.harishkannarao.springboot.graphqlwebmvc.runner;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.util.TestSocketUtils;
 
 import java.util.Properties;
 
@@ -23,8 +24,11 @@ public class ApplicationLocalRunner {
 
 	@NotNull
 	private static Properties createProperties() {
+		final int RANDOM_WIREMOCK_PORT = TestSocketUtils.findAvailableTcpPort();
+
 		final Properties properties = new Properties();
 		properties.setProperty("server.port", "8080");
+		properties.setProperty("wiremock.port", String.valueOf(RANDOM_WIREMOCK_PORT));
 		properties.setProperty("spring.profiles.active", "it");
 		properties.setProperty("app.datasource.hikari.jdbc-url", PostgresTestRunner.getJdbcUrl());
 		properties.setProperty("app.datasource.hikari.username", PostgresTestRunner.getUsername());
